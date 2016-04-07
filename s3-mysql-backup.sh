c2c9fbe9b609df067a7ef0edd4a0abb6c8d44ffc
Jason Diehl <jason.diehl@turbowebs.com>
2016-04-07 16:16:55 -0500

    Update s3-mysql-backup.sh to aws-cli
    Moved to some new hosting and found the S3CMD has a few Python requirements that I couldn't meet and decided to switch to AWS-CLI. Seems a little easier and thorough.

--- s3-mysql-backup.sh
+++ s3-mysql-backup.sh
@@ -1,38 +1,47 @@
 #!/bin/sh
 
+#### s3-mysql-backup.sh  ####
 #### BEGIN CONFIGURATION ####
 
 # set dates for backup rotation
-NOWDATE=`date +%Y%m%d`
+NOWDATE=$(date +%Y%m%d)
 LASTDATE=$(date +%Y%m%d --date='4 weeks ago')
 
 # set backup directory variables
-SRCDIR='/root/s3backups'
-DESTDIR='domain.com'
-BUCKET='BackupBucket'
+MYUSER=""
+MYPASS=""
+SRCDIR="/home/turbowebs/s3backups"
+DESTDIR="$1/db"
+BUCKET="TurboBackups"
 
 #### END CONFIGURATION ####
+
+# check a parameter was sent
+if [ -z "$1" ]
+then
+    echo "Please include directory to backup as parameter"
+    exit
+fi
 
 # make the temp directory if it doesn't exist
 mkdir -p $SRCDIR
 
 # repair, optimize, and dump each database to its own sql file
-for DB in $(mysql -BNe 'show databases' | grep -Ev 'test|mysql|information_schema|performance_schema')
+for DB in $(mysql -u $MYUSER -p$MYPASS -BNe 'show databases' | grep -Ev 'test|mysql|information_schema|performance_schema')
 do
-mysqldump --quote-names --create-options --add-drop-table --force $DB > $SRCDIR/$DB.sql
-mysqlcheck --auto-repair --optimize $DB
+mysqldump -u $MYUSER -p$MYPASS --quote-names --create-options --add-drop-table --force "$DB" > "$SRCDIR/$DB.sql"
+mysqlcheck -u $MYUSER -p$MYPASS --auto-repair --optimize "$DB"
 done
 
 # tar all the databases into $NOWDATE-backups.tar.gz
-cd $SRCDIR
-tar -czPf $NOWDATE-db-backup.tar.gz *.sql
+cd $SRCDIR || exit
+tar -czPf "$NOWDATE-db-backup.tar.gz" ./*.sql
 
 # upload backup to s3
-s3cmd put $SRCDIR/$NOWDATE-db-backup.tar.gz s3://$BUCKET/$DESTDIR/
+aws s3 cp "$NOWDATE-db-backup.tar.gz" "s3://$BUCKET/$DESTDIR/"
 
 # delete old backups from s3
-s3cmd del --recursive s3://$BUCKET/$DESTDIR/$LASTDATE-db-backup.tar.gz
+aws s3 rm "s3://$BUCKET/$DESTDIR/$LASTDATE-db-backup.tar.gz"
 
 # remove all files in our source directory
-cd
 rm -f $SRCDIR/*
